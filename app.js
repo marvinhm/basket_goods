4b3208073cfff9e84c4c4da235e779dc93fbe518
marvinhm <marvinmatovu@gmail.com>
2020-01-29 00:44:28 +0000

    add jest, setup templating engine

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 
 // stuff going on outside of requires
 const app = express();
 
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
 app.use(morgan('tiny'));
 app.get('/', (req, res) => {
-  res.send('Welcome to the Groccery');
+  res.render('index');
 });
 
 app.listen(5000, () => {
